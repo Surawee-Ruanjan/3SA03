d3d34223a13861191220f29bc13ae714cb6e0c25
Surawee-Ruanjan <surawee05@gmail.com>
2019-09-23 15:35:05 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+import CharacterCard from './CharacterCard';
+import _ from 'lodash';
+
+let message = 'Hello'
+
+const prepareStateFromWord = (given_word) => {
+  let word = given_word.toUpperCase()
+  let chars = _.shuffle(Array.from(word))
+  return {
+    word,
+    chars,
+    attempt: 1,
+    guess: [],
+    completed: false
+  }
 }
+
+class App extends Component {
+  
+  state = prepareStateFromWord(message);
+  
+  activationHandler = (c) => {
+      let guess = [...this.state.guess, c]
+      this.setState({guess})
+      if(guess.length == this.state.chars.length){
+        if(guess.join('').toString() == this.state.word){
+          this.setState({guess: [], completed: true})
+        }else{
+          this.setState({guess: [], attempt: this.state.attempt + 1})
+        }
+      }
+  }
+render() {
+  return (
+    <div>
+      {
+        Array.from(this.state.chars).map((item, index) => (
+          <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+              />
+            )
+          )
+        }
+      </div>
+    )  
+  }
+}
+
 export default App;
+
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-export default class CharacterCard extends Component {
-    render(){
-    return (
-    <div>{this.props.value}</div>
-    )
+
+class CharacterCard extends Component {
+    
+    state = {
+        active: false,
     }
-}
    
+    activate = () => {
+        this.setState({
+            active: !this.state.active
+        });
+
+        this.props.activationHandler(this.props.value);
+    }
+    
+    render(){
+        let className = `card ${this.state.active ? 'activeCard': ''}`
+            return (
+                <div className={className} onClick={this.activate}>
+                    {this.props.value}
+                </div>
+            )
+        }
+    }
+       
+
+export default CharacterCard;
